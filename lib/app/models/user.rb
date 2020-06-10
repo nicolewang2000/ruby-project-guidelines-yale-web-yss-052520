f93b49635315a5e036abb7de2920ef2f62206cc9
zayzay500 <lindsay.pierce@yale.edu>
2020-06-10 12:05:00 -0400

    added methods to user class to delete past/particular reservations

--- lib/app/models/user.rb
+++ lib/app/models/user.rb
@@ -7,8 +7,26 @@ class User < ActiveRecord::Base
         Reservation.new(guest_number: guest_number, user_id: self.id, business_id: business_id, date: date)
     end
 
+    def confirm_reservation(reservation)
+        self.reservations.push(reservation)
+        reservation.save
+    end
+
     # accepts API business hash to create new business instance
-    def new_business(business)
-        Business.new(yelp_business_id: business["id"], name: business["name"], address: business["display_address"], avg_rating: business["rating"], review_count: business["review_count"])
+    def new_business_from_API(business)
+        self.businesses.push(Business.create(yelp_business_id: business["id"], name: business["name"], address: business["display_address"], avg_rating: business["rating"], review_count: business["review_count"]))
+    end
+
+    def clear_past_reservations
+        self.reservations.each do |reservation| 
+            if reservation.past?
+                delete_reservation(reservation)
+            end
+        end
+    end
+
+    def delete_reservation(reservation)
+        self.reservations.delete(reservation)
+        reservation.delete
     end
 end
