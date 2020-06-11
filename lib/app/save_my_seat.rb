557b5beceb47e5df6e81a798e8cf3dd789110a0c
zayzay500 <lindsay.pierce@yale.edu>
2020-06-11 10:06:58 -0400

    cli outline

---
+++ lib/app/save_my_seat.rb
@@ -0,0 +1,69 @@
+class SaveMySeat
+
+    def run
+        # Outline
+
+        # Welcome! 
+        # - ask if you have an account
+        #   - if yes
+        #       - get username
+        #       - check if username exists
+        #       - either prompt for password or prompt to create account (User does not exist)
+        #   - if no
+        #       - prompt to create account
+        
+        # Logged In!
+        # - Would you like to view your reservations?
+        # - Would you like to execute a search?
+
+        # Business Search
+        # - To search businesses: Ask for current location + cuisine type
+        #   - execute search (create business instances for results)
+        #   - prompt user to choose sorting method
+        #   - print out search list (self.list_of_businesses)
+        #   - prompt user to execute new search/different sorting method/make reservation
+
+        # def business_search
+        #   helper methods that do the things we outline
+        # end
+
+        # Manage Reservations
+        # - list reservations or say "you have no reservations at this time"
+        # - Give user option to delete reservations
+
+        # Choosing Business for Reservation
+        # - Prompt user to choose a business
+        # - Retrieve and display business hours
+        # - Prompt user to choose reservation date/guest_number
+        #   - check if reservation is valid
+        # - Once validity is good, ask user to confirm
+        # - Once confirmed, persist reservation to database
+
+
+        # Pseudocode
+
+        # welcome
+        # login page
+
+
+    end
+
+    private
+
+    # def login_page
+    # prompt: manage reservation or business search
+    # business search
+
+    # manage reservation
+
+    # def business_search(location, cuisine type)
+    #   helper methods that do the things we outline
+    #   execute search(location, cuisine type) 
+    # end
+
+    # def choose_sorting_method
+    # 
+    # end
+
+
+end
